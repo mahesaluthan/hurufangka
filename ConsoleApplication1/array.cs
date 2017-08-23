2688c648fb8a4fd2e0e5b8d2777783f37478072b
mahesaluthan <echasmith97@gmail.com>
2017-08-23 10:29:19 +0700

    console

---
+++ ConsoleApplication1/array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class echa
+    {
+        public void Echasmith(params int[] echa)
+        {
+            foreach (int i in echa)
+            {
+                Console.WriteLine("Echasmith {0}", i);
+            }
+        }
+        static void Main()
+        {
+            echa e = new echa();
+            e.Echasmith(5, 6, 7, 8);
+            int[] explicitArray = new int[5] { 1, 2, 3, 4, 5 };
+            e.Echasmith(explicitArray);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication7/Huruf angka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Huruf;
+            int angka;
+
+            Console.Write("input: ");
+            Huruf = Console.ReadLine();
+            angka = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Itu adalah huruf");
+            /*if (Huruf >= 'A' && Huruf <= 'Z' )
+            {
+                Console.WriteLine("Itu adalah huruf kapital");
+            }
+            else if (Huruf >= 'a' && Huruf <= 'z')
+            {
+                Console.WriteLine("Itu adalah huruf kecil ");
+            }*/
+             if (angka >= '0' && angka <= '9' )
+            {
+                Console.WriteLine("Itu adalah angka");
+            }
+            else
+            {
+                Console.WriteLine("itu adalah simbol");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
